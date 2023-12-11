d5eca1f56158c121b5cab71a36729ebd93d4bbd3
tommg <tommg@Air.local>
2023-12-11 15:22:39 +0000

    fixed writing to proper file name

--- main.c
+++ main.c
@@ -12,16 +12,35 @@ int main(int argc, char **argv){
 	int nameLen = strlen(argv[1]);
 
 	char fileExt[5] = ".tex";
-	char fileName[4+nameLen];
+//	char fileName[4+nameLen];
+	char fileName[50];
 
-	strcpy(fileName, argv[1]);
-	strcpy(fileName, fileExt);
-	printf("%s", fileName);
+	strcat(fileName, argv[1]);
+	strcat(fileName, fileExt);
+	printf("%s\n", fileName);
 
+//	char texData[80] =
+//"\\documentclass{article}\n\n
+//\\usepackage{graphicx}\n
+//\\usepackage{float}\n
+//\\usepackage{amsmath}\n
+//\\usepackage{amssymb}\n
+//\\usepackage{bbm}\n\n
+//\\graphicspath{{./images}}\n\n
+//\\begin{document}\n\n
+//\\end{document}\n";
+	
 
-//	FILE *fptr
-//	
-//	fptr = fopen()
+	FILE *fptr;
+	
+	fptr = fopen(fileName, "w");
+	printf("%s file created\n", fileName);
+
+	fprintf(fptr, "\\documentclass{article}\n");
+	printf("test data written to %s file\n", fileName);
+
+	fclose(fptr);
+	printf("%s closed\n", fileName);
 
 	return 0;
 	}
